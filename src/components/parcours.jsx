436021e81627b025adcbd66b8238190fd573fe53
ElMonkito <rh@RT-QLS-NBK-022.ad.reist-tele.com>
2025-02-27 13:49:37 +0100

    change const name of jobs

--- src/components/parcours.jsx
+++ src/components/parcours.jsx
@@ -3,9 +3,8 @@ import {Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineCo
 import WorkIcon from '@mui/icons-material/Work';
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import {createTheme, ThemeProvider} from "@mui/material";
 
-const educationStages = [
+const jobs = [
     {title: "École Primaire", name: "Attalens", year: "2010 - 2017"},
     {title: "École Secondaire", name: "Cycle d'orientation de la Veveyse", year: "2018 - 2021"},
     {title: "CFC Developpement d'applications", name: "Bern, SBB-CFF-FFS", year: "août 2021 - juillet 2024"},
@@ -13,41 +12,41 @@ const educationStages = [
 ];
 
 export default function EducationTimeline() {
-    const theme = createTheme({
-        typography: {
-            fontFamily: "Rubik",
-        },
-    });
 
     return (
-        <ThemeProvider theme={theme}>
-            <Box sx={{
-                backgroundColor: "#0D1321",
-                height: {xs: "120vh", md: "65vh"},
-                display: "flex",
-                marginTop: {xs: "10%", md: "3%"},
-                flexDirection: {xs: "column", md: "row"},
-                overflow: "hidden",
-                paddingBottom: {xs: "0%", md: "3%"}
-            }}>
-                <Timeline position="alternate" sx={{marginTop: {xs: "15%", md: "3%"}}}>
-                    {educationStages.map((stage, index) => (
-                        <TimelineItem key={index}>
-                            <TimelineSeparator>
-                                <TimelineDot color="primary">
-                                    <WorkIcon sx={{ color: "#C5D86D"}}/>
-                                </TimelineDot>
-                                {index < educationStages.length - 1 && <TimelineConnector sx={{ height: {xs: "100px", md: "180px"} }}/>}
-                            </TimelineSeparator>
-                            <TimelineContent sx={{color: "white"}}>
-                                <Typography variant="h6" sx={{color: "#C5D86D", fontSize: {xs: "1.3rem", md: "1.5rem"} }}>{stage.title}</Typography>
-                                <Typography color="textSecondary" sx={{color: "#FFEDDF", fontSize: {xs: "0.9rem", md: "1.2rem"} }}>{stage.name}</Typography>
-                                <Typography color="textSecondary" sx={{color: "#FFEDDF"}}>{stage.year}</Typography>
-                            </TimelineContent>
-                        </TimelineItem>
-                    ))}
-                </Timeline>
-            </Box>
-        </ThemeProvider>
+        <Box sx={{
+            backgroundColor: "#0D1321",
+            height: {xs: "120vh", md: "65vh"},
+            display: "flex",
+            marginTop: {xs: "10%", md: "3%"},
+            flexDirection: {xs: "column", md: "row"},
+            overflow: "hidden",
+            paddingBottom: {xs: "0%", md: "3%"}
+        }}>
+            <Timeline position="alternate" sx={{marginTop: {xs: "15%", md: "3%"}}}>
+                {jobs.map((stage, index) => (
+                    <TimelineItem key={index}>
+                        <TimelineSeparator>
+                            <TimelineDot color="primary">
+                                <WorkIcon sx={{color: "#C5D86D"}}/>
+                            </TimelineDot>
+                            {index < jobs.length - 1 &&
+                                <TimelineConnector sx={{height: {xs: "100px", md: "180px"}}}/>}
+                        </TimelineSeparator>
+                        <TimelineContent sx={{color: "white"}}>
+                            <Typography variant="h6" sx={{
+                                color: "#C5D86D",
+                                fontSize: {xs: "1.3rem", md: "1.5rem"}
+                            }}>{stage.title}</Typography>
+                            <Typography color="textSecondary" sx={{
+                                color: "#FFEDDF",
+                                fontSize: {xs: "0.9rem", md: "1.2rem"}
+                            }}>{stage.name}</Typography>
+                            <Typography color="textSecondary" sx={{color: "#FFEDDF"}}>{stage.year}</Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+                ))}
+            </Timeline>
+        </Box>
     );
 }
