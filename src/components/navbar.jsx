2799d4732b5703c75c89a074a6753e0c1d5239e2
ElMonkito <rh@RT-QLS-NBK-022.ad.reist-tele.com>
2025-02-27 14:28:57 +0100

    center nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -68,7 +68,6 @@ export default function Navbar() {
                         </Box>
                     </Toolbar>
                 </AppBar>
-
                 <Drawer
                     anchor="right"
                     open={mobileOpen}
